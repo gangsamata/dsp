305d51f57415836d94a537461733ab07a7535a1e
gangsamata <118532851+gangsamata@users.noreply.github.com>
2023-05-23 17:54:13 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,12 @@
+import import java.rmi.*;
+
+public interface Concat extends Remote
+{  
+	public String concat(String x,String y)throws RemoteException;  
+}  
+java.rmi.*;
+
+public interface Concat extends Remote
+{  
+	public String concat(String x,String y)throws RemoteException;  
+}  
