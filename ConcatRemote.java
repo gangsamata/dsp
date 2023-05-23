76245d2393e79055b72299df72ec271595d7defc
gangsamata <118532851+gangsamata@users.noreply.github.com>
2023-05-23 17:52:45 +0530

    Create ConcatRemote.java

---
+++ ConcatRemote.java
@@ -0,0 +1,13 @@
+import java.rmi.*;  
+import java.rmi.server.*;  
+public class ConcatRemote extends UnicastRemoteObject implements Concat
+{  
+	ConcatRemote()throws RemoteException
+	{  
+	    super();  
+	}  
+    	public String concat(String x,String y)
+	{
+		return x+y;
+    	}  
+}  
